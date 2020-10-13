e5d28e1017ad0bff476c074d92194d469580a0f1
uzytkownik <danio9996@o2.pl>
2020-10-13 14:50:38 +0200

    added fourth podcast test

--- Desktop/mmm/seleniumZDAUTpol2kurs/src/test/java/BaseTest.java
+++ Desktop/mmm/seleniumZDAUTpol2kurs/src/test/java/BaseTest.java
@@ -6,6 +6,7 @@ import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.WebDriverWait;
 
 import java.util.List;
+import java.util.concurrent.TimeUnit;
 
 import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertTrue;
@@ -15,27 +16,29 @@ public class BaseTest {
     WebDriver driver;
     WebDriverWait wait;
 
-    public void highlightElement(WebDriver driver, WebElement element){
+    public void highlightElement(WebDriver driver, WebElement element) {
         JavascriptExecutor js = (JavascriptExecutor) driver;
         js.executeScript("arguments[0].setAttribute('style', 'background: green; border: 3px solid blue;');", element);
     }
 
     @Before
-    public void setUp(){
-       System.setProperty("webdriver.chrome.driver","C:\\chromedriver\\chromedriver.exe");
-       driver = new ChromeDriver();
-       driver.manage().window().maximize();
-       String devToUrl = "https://dev.to";
+    public void setUp() {
+        System.setProperty("webdriver.chrome.driver", "C:\\chromedriver\\chromedriver.exe");
+        driver = new ChromeDriver();
+        driver.manage().window().maximize();
+        String devToUrl = "https://dev.to";
         driver.get(devToUrl);
         wait = new WebDriverWait(driver, 20);
+        driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
     }
+
     @Test
-    public void firsttest(){
+    public void selectFirstPostFromWeek() {
 
         WebElement week = driver.findElement(By.xpath("//a[@href='/top/week']"));
         week.click();
         wait.until(ExpectedConditions.urlToBe("https://dev.to/top/week"));
-        WebElement firstPostOnWeek = driver.findElement(By.cssSelector("h2.crayons-story__title > a" ));
+        WebElement firstPostOnWeek = driver.findElement(By.cssSelector("h2.crayons-story__title > a"));
         String firstPostOnWeekText = firstPostOnWeek.getText();
         String firstPostLink = firstPostOnWeek.getAttribute("href");
         firstPostOnWeek.click();
@@ -48,7 +51,7 @@ public class BaseTest {
         String postTitleText = postTitle.getText();
 
         assertEquals("Urls aren't the same", posturl, firstPostLink);
-        assertEquals("Titles aren't the same",postTitleText, firstPostOnWeekText);
+        assertEquals("Titles aren't the same", postTitleText, firstPostOnWeekText);
 
     }
 
@@ -73,26 +76,27 @@ public class BaseTest {
 //            assertTrue("there's no searching value in post tile", elementText.contains(searchText));
 //        }
         int i = 0;
-        while (i<5){
+        while (i < 5) {
             highlightElement(driver, postTilesList.get(i));
             i++;
         }
-
 
 
         WebElement element = postTilesList.get(4);
         String elementText = element.getText();
         assertTrue("there's no searching value in post tile", elementText.contains(searchText));
     }
+
     @Test
-    public void findJavaInNavBar(){
+    public void findJavaInNavBar() {
         WebElement java = driver.findElement(By.cssSelector(("div#default-sidebar-element-java > a")));
         highlightElement(driver, java);
         java.click();
 
     }
+
     @Test
-    public void findTutorialInNavBar(){
+    public void findTutorialInNavBar() {
         WebElement tutorial = driver.findElement(By.cssSelector(("div#default-sidebar-element-tutorial > a")));
         highlightElement(driver, tutorial);
         tutorial.click();
@@ -100,17 +104,35 @@ public class BaseTest {
         wait.until(ExpectedConditions.urlToBe(tutorialUrl));
         List<WebElement> tutorialList = driver.findElements(By.cssSelector("h2.crayons-story__title > a"));
 
-        int i = 0;
-        while (i<3){
-            highlightElement(driver, tutorialList.get(i));
-            i++;
+
+        }
+
+        @Test
+        public void playFourthPodcast(){
+            WebElement podcast = driver.findElement(By.xpath("//a[@href='/pod']"));
+            podcast.click();
+            wait.until(ExpectedConditions.urlToBe("https://dev.to/pod"));
+            wait.until(ExpectedConditions.visibilityOfAllElementsLocatedBy(By.tagName("h3")));
+            List<WebElement> podcasts = driver.findElements(By.tagName("h3"));
+            podcasts.get(3).click();
+            wait.until(ExpectedConditions.urlContains("stackpodcast"));
+            WebElement playArea = driver.findElement(By.className("record-wrapper"));
+            playArea.click();
+            WebElement initializing = driver.findElement(By.className("status-message"));
+            wait.until(ExpectedConditions.invisibilityOf(initializing));
+            String playAreaClassAttribute = playArea.getAttribute("class");
+            boolean isPlaying = playAreaClassAttribute.contains("playing");
+
+            assertTrue("Podcast isn't playing",isPlaying);
+        }
+
 
     }
 
 
 
-}
-}
+
+
 
 
 
